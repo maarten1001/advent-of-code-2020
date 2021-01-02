289e0ab199e9a2a2bee751ab4ebfe1522d2962a4
Maarten Smeyers <maartensmeyers@gmail.com>
2021-01-02 09:16:57 +0100

    day 11 part 1

---
+++ day11/part1.py
@@ -0,0 +1,58 @@
+# get the input file
+f = open("input.txt")
+seats = f.read().splitlines()
+f.close()
+
+# add padding
+padding = "." * len(seats[0])
+seats.insert(0, padding)
+seats.append(padding)
+for i in range(len(seats)):
+    seats[i] = "." + seats[i] + "."
+
+
+def print_seats(r):
+    occupied = 0
+    print("Round " + str(r) + ":")
+    for i in range(1, len(seats) - 1):
+        print(seats[i][1:-1])
+        occupied += seats[i].count("#")
+    print()
+    print(str(occupied) + " seats are occupied")
+    print()
+
+
+def count_adjacent(x, y):
+    total = 0
+    total += seats[x - 1][y - 1] == "#"
+    total += seats[x - 1][y] == "#"
+    total += seats[x - 1][y + 1] == "#"
+    total += seats[x][y - 1] == "#"
+    total += seats[x][y + 1] == "#"
+    total += seats[x + 1][y - 1] == "#"
+    total += seats[x + 1][y] == "#"
+    total += seats[x + 1][y + 1] == "#"
+    return total
+
+
+rounds = 0
+print_seats(rounds)
+
+while True:
+    rounds += 1
+    changed = False
+    s2 = seats.copy()
+    for i in range(len(s2)):
+        for j in range(len(s2[i])):
+            if seats[i][j] == "L" and count_adjacent(i, j) == 0:
+                s2[i] = s2[i][:j] + "#" + s2[i][j + 1:]
+                changed = True
+            elif seats[i][j] == "#":
+                if count_adjacent(i, j) >= 4:
+                    s2[i] = s2[i][:j] + "L" + s2[i][j + 1:]
+                    changed = True
+    if changed:
+        seats = s2
+        print_seats(rounds)
+    else:
+        break
