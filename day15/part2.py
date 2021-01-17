ee7479f6d39800359bd97cf22b2d95422480fa9e
Maarten Smeyers <maartensmeyers@gmail.com>
2021-01-17 08:45:36 +0100

    Finally with an acceptable runtime

--- day15/part2.py
+++ day15/part2.py
@@ -1,43 +1,56 @@
-# import timeit
-import profile
+# import cProfile
 
 # get the input file
 f = open("input.txt")
 entries = f.read()
 f.close()
 
+# set up a dict where each value is the last index of its key
 num = [int(x) for x in entries.split(",")]
-unique_num = set(num[:-1])
+index_of = dict()
+for ii in range(0, len(num) - 1):
+    index_of.update({num[ii]: ii})
 
 
-def next_number():
-    for n in range(len(num) - 1, 300000):
-        # print(num)
-        # print(unique_num)
-        last = num[n]
-        # print("Last number is " + str(last))
-        # lookup in a set instead of iterating over the list
-        if last not in unique_num:
-            # print("Not seen before, append 0")
-            num.append(0)
-            # print("Add " + str(last) + " to the set")
-            unique_num.add(last)
+def next_number(limit):
+    start = len(num) - 1
+    last = num[start]
+    for n in range(start, limit - 1):
+        if last not in index_of:
+            index_of[last] = n
+            last = 0
         else:
-            for i in range(n - 1, -1, -1):
-                if num[i] == last:
-                    # print("Seen before, append " + str(n-i))
-                    num.append(n - i)
-                    break
+            diff = n - index_of[last]
+            index_of[last] = n
+            last = diff
+    print(last)
 
 
-profile.run('next_number(); print(num[300000 - 1]); print(unique_num); print()')
+next_number(30000000)
 
-# before set lookup
-# range   2020 -  20203 function calls in   4.359 seconds
-# range  20200 - 202003 function calls in 404.016 seconds
+# cProfile.run('next_number(30000000); print()')
 
-# after set lookup
-# range   2020 -   2403 function calls in   0.062 seconds
-# range  20200 -  23520 function calls in   1.438 seconds
-# range 300000 - 343093 function calls in 224.312 seconds
+# traversing the list backwards to find the offset to the previous index
+# range     2020 -    20203 function calls in   4.359 seconds
+# range    20200 -   202003 function calls in 404.016 seconds
+
+# using a set lookup before traversal to check if the number was seen before
+# to prevent traversal of the entire list
+# range     2020 -     2403 function calls in   0.062 seconds
+# range    20200 -    23520 function calls in   1.438 seconds
+# range   300000 -   343093 function calls in 224.312 seconds
 # still too slow
+
+# using a dict to store the previous index of all numbers
+# range     2020 -     2023 function calls in   0.016 seconds
+# range    20200 -    20203 function calls in   0.094 seconds
+# range   300000 -   300003 function calls in   1.219 seconds
+# range 30000000 - 30000003 function calls in 120.266 seconds
+
+# solution = 47205
+
+# no longer expanding the list
+# range     2020 -        8 function calls in   0.016 seconds
+# range    20200 -        8 function calls in   0.031 seconds
+# range   300000 -        8 function calls in   0.109 seconds
+# range 30000000 -        8 function calls in  14.469 seconds
