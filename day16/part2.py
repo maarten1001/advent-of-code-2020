d6ab2531a8231571bf9a39d3f94719ec76c9f5bf
Maarten Smeyers <maartensmeyers@gmail.com>
2021-01-22 08:49:52 +0100

    day 16 part 2

--- day16/part2.py
+++ day16/part2.py
@@ -1,14 +1,17 @@
 import re
 
 # get the input file
-f = open("test.txt")
+f = open("input.txt")
 lines = f.read()
 f.close()
 
 lines = lines.split("\n\n")
 rules = lines[0].split("\n")
 ticket = lines[1].split("\n")[1]
+ticket = [int(x) for x in ticket.split(",")]
 nearby = lines[2].split("\n")[1:]
+for i in range(len(nearby)):
+    nearby[i] = [int(x) for x in nearby[i].split(",")]
 
 # process the rules into a set of fields and valid values
 valid = set()
@@ -26,18 +29,37 @@ error_rate = 0
 # discard invalid tickets
 # https://sopython.com/canon/95/removing-items-from-a-list-while-iterating-over-the-list/
 for t in nearby.copy():
-    values = [int(x) for x in t.split(",")]
-    for v in values:
-        if v not in valid:
-            error_rate += v
+    for value in t:
+        if value not in valid:
+            error_rate += value
             nearby.remove(t)
             break
 
-# determine the order of the fields
-possible_fields = [set(fields.keys()) for x in fields]
-print(possible_fields)
+possible_fields = [list(fields.keys()) for x in fields]
 
-print("The error rate is " + str(error_rate))
+# determine the order of the fields
+while True:
+    changed = False
+    for t in nearby:
+        for i in range(len(t)):
+            for key, values in fields.items():
+                if t[i] not in values:
+                    if key in possible_fields[i]:
+                        possible_fields[i].remove(key)
+                    # if a field has been identified, remove that option from the other fields
+                    if len(possible_fields[i]) == 1:
+                        unique_field = possible_fields[i][0]
+                        for p in possible_fields:
+                            if len(p) > 1 and unique_field in p:
+                                p.remove(unique_field)
+                                changed = True
+    if not changed:
+        break
+
+product = 1
+for i in range(len(possible_fields)):
+    if "departure" in possible_fields[i][0]:
+        product *= int(ticket[i])
+    print(str(i + 1) + " - " + str(possible_fields[i]))
 print()
-print("Valid tickets:")
-print(nearby)
+print(product)
